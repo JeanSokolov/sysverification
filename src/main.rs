f4e77e674fab22f8306ed2e9646d05f7465e9e5f
JeanSokolov <jean.sokolov@stud.hshl.de>
2024-02-06 17:51:17 +0100

    Added optional arguments for num of gcs (arg 2) and bcs (arg 3)

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,14 @@ use std::{
 mod parser;
 
 fn main() -> std::io::Result<()> {
-    let num_simulation_cycles = 4;
+    let gcs_simulation_cycles: i32 = std::env::args().nth(2).unwrap().parse().unwrap();
+    let bcs_simulation_cycles: i32 = std::env::args().nth(3).unwrap().parse().unwrap();
+
+    println!(
+        "Performing {} cycles with {} stuck ats each",
+        gcs_simulation_cycles, bcs_simulation_cycles
+    );
+
     let mut data = json::JsonValue::new_object();
 
     let filepath = std::env::args()
@@ -17,87 +24,92 @@ fn main() -> std::io::Result<()> {
     let f = File::open(filepath)?;
     let reader = BufReader::new(f);
 
-    let mut gcs_names: Vec<String> = Vec::new();
-    let mut gcs_values: Vec<Option<bool>> = Vec::new();
-    let mut stuck_ats: Vec<String> = Vec::new();
     let (mut network, mut gates, mut outputs) = parse(reader);
-    let mut iteration_input_values: Vec<bool> = Vec::new();
+    let mut stuck_ats: Vec<String> = Vec::new();
 
-    // generate random input values
-    for (_, gate) in network.iter_mut() {
-        let mut gate = gate.lock().unwrap(); // Acquire lock
-        if gate.input.iter().any(|item| item.is_none()) {
-            let rdm = rand::thread_rng().gen::<bool>();
-            iteration_input_values.push(rdm);
-            gate.value = Some(rdm);
-            data["test_pattern"][&gate.name] = (rdm as i32).into();
-        }
-    }
-
-    // good circuit simulation loop
-    for i in gates.clone() {
-        let mut gate = network[&i].lock().unwrap(); // Acquire lock
-        gate.value = gate.evaluate();
-        gcs_values.push(gate.value.clone());
-        gcs_names.push(gate.name.clone());
-        data["gcs"][&gate.name] = (gate.value.unwrap() as i32).into();
-    }
-
-    for n in 0..num_simulation_cycles {
-        let mut bcs_values: Vec<Option<bool>> = Vec::new();
-        // Setting stuck at
+    for l in 0..gcs_simulation_cycles {
+        let mut gcs_names: Vec<String> = Vec::new();
+        let mut gcs_values: Vec<Option<bool>> = Vec::new();
+        let mut iteration_input_values: Vec<bool> = Vec::new();
+        // generate random input values
         for (_, gate) in network.iter_mut() {
-            let mut unlocked_gate = gate.lock().unwrap();
-            if unlocked_gate.kind.eq(&1) {
-                if rand::thread_rng().gen::<bool>() {
-                    if !stuck_ats.contains(&unlocked_gate.name) {
-                        let rdm = rand::thread_rng().gen::<bool>();
-                        println!(
-                            "Setting stuck-at for input[0] at {} for Gate: {}",
-                            rdm as i32, unlocked_gate.name
-                        );
-                        unlocked_gate.stuck_at = vec![Some(rdm), None];
-                        stuck_ats.push(unlocked_gate.name.clone());
-                        data[&format!("bcs_iteration_{}", n)]["stuck_at"] =
-                            (format!("input[0]={} for Gate: {}", rdm as i32, unlocked_gate.name))
-                                .into();
-                        break;
-                    }
-                }
+            let mut gate = gate.lock().unwrap(); // Acquire lock
+            if gate.input.iter().any(|item| item.is_none()) {
+                let rdm = rand::thread_rng().gen::<bool>();
+                iteration_input_values.push(rdm);
+                gate.value = Some(rdm);
+                data[&l.to_string()]["test_pattern"][&gate.name] = (rdm as i32).into();
             }
         }
-        // bad circuit simulation loop
+
+        // good circuit simulation loop
         for i in gates.clone() {
             let mut gate = network[&i].lock().unwrap(); // Acquire lock
             gate.value = gate.evaluate();
-            bcs_values.push(gate.value.clone());
+            gcs_values.push(gate.value.clone());
+            gcs_names.push(gate.name.clone());
+            data[&l.to_string()]["gcs"][&gate.name] = (gate.value.unwrap() as i32).into();
         }
 
-        // If results of good circuit simulation are different from results of bad circuit simulation, output all values that differ
-        if gcs_values.ne(&bcs_values) {
-            for i in 0..gcs_names.clone().len() {
-                if gcs_values[i].ne(&bcs_values[i]) {
-                    println!(
-                        "GATE: {}, GCS: {:?}, BCS: {:?}, is_output: {:?}",
-                        gcs_names[i],
-                        gcs_values[i],
-                        bcs_values[i],
-                        outputs.contains(&gcs_names[i])
-                    );
+        for n in 0..bcs_simulation_cycles {
+            let mut bcs_values: Vec<Option<bool>> = Vec::new();
+            // Setting stuck at
+            for (_, gate) in network.iter_mut() {
+                let mut unlocked_gate = gate.lock().unwrap();
+                if unlocked_gate.kind.eq(&1) {
+                    if rand::thread_rng().gen::<bool>() {
+                        if !stuck_ats.contains(&unlocked_gate.name) {
+                            let rdm = rand::thread_rng().gen::<bool>();
+                            println!(
+                                "Setting stuck-at for input[0] at {} for Gate: {}",
+                                rdm as i32, unlocked_gate.name
+                            );
+                            unlocked_gate.stuck_at = vec![Some(rdm), None];
+                            stuck_ats.push(unlocked_gate.name.clone());
+                            data[&l.to_string()][&format!("bcs_iteration_{}", n)]["stuck_at"] =
+                                (format!(
+                                    "input[0]={} for Gate: {}",
+                                    rdm as i32, unlocked_gate.name
+                                ))
+                                .into();
+                            break;
+                        }
+                    }
                 }
-                data[&format!("bcs_iteration_{}", n)][&gcs_names[i]] =
-                    (bcs_values[i].unwrap() as i32).into();
             }
-        } else {
-            data[&format!("bcs_iteration_{}", n)]["result"] = ("same as gcs").into();
-            println!("Stuck-at of iteration: {} had no impact", n);
+            // bad circuit simulation loop
+            for i in gates.clone() {
+                let mut gate = network[&i].lock().unwrap(); // Acquire lock
+                gate.value = gate.evaluate();
+                bcs_values.push(gate.value.clone());
+            }
+
+            // If results of good circuit simulation are different from results of bad circuit simulation, output all values that differ
+            if gcs_values.ne(&bcs_values) {
+                for i in 0..gcs_names.clone().len() {
+                    if gcs_values[i].ne(&bcs_values[i]) {
+                        println!(
+                            "GATE: {}, GCS: {:?}, BCS: {:?}, is_output: {:?}",
+                            gcs_names[i],
+                            gcs_values[i],
+                            bcs_values[i],
+                            outputs.contains(&gcs_names[i])
+                        );
+                    }
+                    data[&l.to_string()][&format!("bcs_iteration_{}", n)][&gcs_names[i]] =
+                        (bcs_values[i].unwrap() as i32).into();
+                }
+            } else {
+                data[&l.to_string()][&format!("bcs_iteration_{}", n)]["result"] =
+                    ("same as gcs").into();
+                println!("Stuck-at of iteration: {} had no impact", n);
+            }
+
+            println!("\n-------------------------\n");
+
+            // clear stuck at
+            network[stuck_ats.last().unwrap()].lock().unwrap().stuck_at = vec![None, None];
         }
-
-        println!("\n-------------------------\n");
-
-        // clear stuck at
-        network[stuck_ats.last().unwrap()].lock().unwrap().stuck_at = vec![None, None];
-
         // Print Outputs
         /* for (name, gate) in network.iter() {
             let gate = gate.lock().unwrap(); // Acquire lock
